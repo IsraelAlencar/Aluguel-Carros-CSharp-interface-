bfb875f23a8bebf3f408f34e05f5d38ea5a6c1e0
IsraelAlencar <israelalencarribeiro@mgmail.com>
2021-12-14 10:45:07 -0400

    Aluguel de carros (INTERFACE)

---
+++ AluguelDeCarros/Services/BrazilTaxService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AluguelDeCarros.Services {
+    class BrazilTaxService {
+        public double Amount { get; set; }
+
+        public 
+    }
+}
